c5e7f2edb9597a5d0e08cf39d0dd93e953713ce8
MangaVault10x <carmine.montella.arcaplanet@gmail.com>
2026-05-14 16:30:36 +0200

    Refactor manga.js to improve structure and readability

--- routes/manga.js
+++ routes/manga.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../db");
+const { translateToItalian } = require("../services/translate");
 
 //
 // AUTO ENRICH
