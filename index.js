71a4ca0544a5fe3fb5351c4afd7ef6ae20809825
MangaVault10x <carmine.montella.arcaplanet@gmail.com>
2026-05-27 09:25:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ const wishlistRouter = require("./routes/wishlist");
 app.use("/api/wishlist", wishlistRouter);
 
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
+
 app.use(express.json());
 
 // importa le tue route esistenti
