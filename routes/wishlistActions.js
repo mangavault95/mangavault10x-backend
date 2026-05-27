f813f12c78c2de4d80036ec001691cd2d3f4b93f
MangaVault10x <carmine.montella.arcaplanet@gmail.com>
2026-05-27 16:52:50 +0200

    Update wishlistActions.js

--- routes/wishlistActions.js
+++ routes/wishlistActions.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../db");
-
+ 
 // POST /api/wishlist-actions/purchase/:id
 router.post("/purchase/:id", async (req, res) => {
   const client = await pool.connect();
