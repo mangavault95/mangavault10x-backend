077f1fbf1512b92df5292ae0f4a793b572fcb90a
MangaVault10x <carmine.montella.arcaplanet@gmail.com>
2026-05-27 09:49:59 +0200

    Update wishlist.js

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -25,6 +25,19 @@ router.post("/", async (req, res) => {
     return res.status(500).json({ error: "Errore server" });
   }
 });
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await pool.query(
+      "DELETE FROM wishlist_custom WHERE id=$1",
+      [req.params.id]
+    );
+    res.json({ success: true });
+  } catch (err) {
+    res.status(500).json({ error: "Errore delete" });
+  }
+});
+``
 // GET ALL WISHLIST
 router.get("/all", async (req, res) => {
   try {
